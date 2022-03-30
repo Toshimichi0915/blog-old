50b6ca1aca3dbb8c78cf376e090a835753228b44
Toshimichi0915 <main@toshimichi.net>
2022-03-31 03:41:44 +0900

    Add title/viewport

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,19 @@
+import Head from "next/head"
 import "../styles/global.scss"
 
 export default function Blog({ Component, pageProps }) {
-  return <Component {...pageProps} />
+
+  let post
+  if (typeof pageProps["posts"] === "object" && typeof pageProps["post"] === "string") {
+    post = pageProps["posts"][pageProps["post"]].name
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Toshimichi blog {post}</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
