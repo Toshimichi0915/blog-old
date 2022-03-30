f3287d3b6d06d48ed422892ffbfae2cc0719fa76
Toshimichi0915 <main@toshimichi.net>
2022-03-31 00:42:23 +0900

    Fix navbar click area

--- pages/components/navbar.js
+++ pages/components/navbar.js
@@ -18,13 +18,13 @@ export default function Navbar({ name }) {
       <div className="fixed w-full h-12 gap-3 flex items-center justify-center bg-gradient-to-r from-blue-500 to-blue-400">
         {Object.keys(navigation).map(item => {
           return (
-            <p
-              className={classNames("rounded-md py-1 px-3 text-white hover:bg-emerald-400 ease-linear duration-150", { "bg-blue-400": item === name })}
-              key={item}>
-              <Link href={navigation[item].link}>
-                <a>{navigation[item].name}</a>
-              </Link>
-            </p>
+            <Link key={item} href={navigation[item].link}>
+              <a>
+                <p
+                  className={classNames("rounded-md py-1 px-3 text-white hover:bg-emerald-400 ease-linear duration-150",
+                    { "bg-blue-400": item === name })}>{navigation[item].name}</p>
+              </a>
+            </Link>
           )
         })}
       </div>
